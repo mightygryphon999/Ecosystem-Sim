8f327fd5f1726c809fa710d2839f8a8d8816d1af
mightygryphon999 <griffin.roberts.srq@gmail.com>
2026-02-17 22:10:39 -0500

    Bump AssemblyInformationalVersion and rebuild
    Update AssemblyInformationalVersion to c481050abd3d97eeb31625995be466a6ed799ad9 and update SourceLink to reference the same commit. Resulting rebuild refreshed DLLs, PDBs, ref/refint binaries and the AssemblyInfo inputs cache (generated build artifacts).

--- obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcosystemSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0a10e8fbf8d5a29134b8bbe0dc017d6aa9d10d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c481050abd3d97eeb31625995be466a6ed799ad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcosystemSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
