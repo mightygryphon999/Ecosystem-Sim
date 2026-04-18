45600051350391f376a0cf4567cf151a2d134453
mightygryphon999 <griffin.roberts.srq@gmail.com>
2026-04-18 09:39:32 -0400

    Push chunk to list and add neighbor loop
    While iterating world chunks, push the current Chunk into the chunks vector and record its index. Inserted a skeleton for iterating c.cached_chunks so neighbor data can be processed and linked to the newly added chunk (the last two struct fields are currently zeroed as placeholders). This prepares the code for transferring cached neighbor information into the chunks array.

--- CppVersion/src/Compute/OpenGL/Simulation/OpenGLDataStructsTransfer.cpp
+++ CppVersion/src/Compute/OpenGL/Simulation/OpenGLDataStructsTransfer.cpp
@@ -16,6 +16,13 @@ namespace OpenGLDataStructsTransfer {
         for (int x = 0; x < World::world_map_size; x++) {
             for (int y = 0; y < World::world_map_size; y++) {
                 ChunkData::Chunk c = World::chunks_map_main[x][y];
+
+                chunks.push_back(Chunk{c.x, c.y, c.Cw, c.id, 0, 0});
+                int id = chunks.size() - 1;
+
+                for (int chunk_neighbor_range_index = 0; chunk_neighbor_range_index < c.cached_chunks.size(); chunk_neighbor_range_index++) {
+                    
+                }
             }
         }
     }
